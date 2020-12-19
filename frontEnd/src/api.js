7879aeaf8a09639d9744cdabbea1b9271166f0db
ShiwaniBahadur <shiwanibahadur@gmail.com>
2020-12-19 21:08:38 +0530

    commit

--- frontEnd/src/api.js
+++ frontEnd/src/api.js
@@ -4,7 +4,7 @@ import { apiUrl } from './config';
 export const getProduct = async (id) => {
     try {
       const response = await axios({ 
-        url: `${apiUrl}/api/products/ethnicWear${id}`,
+        url: `${apiUrl}/api/products/ethnicWear/${id}`,
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
