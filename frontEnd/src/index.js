869c385055ee0b348c34bc370b7cea5be062ed0e
ShiwaniBahadur <shiwanibahadur@gmail.com>
2020-12-20 14:28:06 +0530

    commit

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -2,10 +2,13 @@ import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 import Error404Screen from './Screens/Error404Screen';
 import { parseRequestUrl } from './utils';
+import CartScreen from './Screens/CartScreen';
 
 const routes = {
     '/': HomeScreen,
     '/product/:id': ProductScreen,
+    '/cart/:id': CartScreen,
+    '/cart': CartScreen,
 };
 const router = async () => { // used to define the url path when a product is opened
     const request = parseRequestUrl();
@@ -16,6 +19,7 @@ const router = async () => { // used to define the url path when a product is op
     const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
     const main = document.getElementById('main-container');
     main.innerHTML = await screen.render();
+    await screen.after_render();
 };
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
